cba02b9ff4ccba1a7d547a3ee1e8210a5fd46fb3
leomata98 <leomata97@gmail.com>
2017-11-19 09:40:13 -0600

    Registro cambios
    Registro de cambios con validaciones

--- proyecto2/RegistrarCambios.cs
+++ proyecto2/RegistrarCambios.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 using System.Data.Sql;
 using Oracle.DataAccess.Client; // ODP.NET Oracle managed provider
 using Oracle.DataAccess.Types;
@@ -86,37 +87,72 @@ namespace proyecto2
 
         private int cantidadCambio(string equipo)
         {
+            /*  string oradb = "DATA SOURCE=localhost:1521/XE;USER ID=SYSTEM; Password = toma;";
+              int aux=0;
+              OracleConnection conn = new OracleConnection(oradb); // C#
+
+              conn.Open();
+
+              try
+              {
+                  OracleCommand cmd = new OracleCommand();
+
+                  cmd.Connection = conn;
+                  cmd.CommandText = " select cambios.idequipo from cambios,jugador where cambios.idequipo = '" + equipo + "' and idpartido = " + idPartido +" and cambios.idjugador = jugador.pasaporte ";
+                   string instruccion =  " select cambios.idequipo from cambios,jugador where cambios.idequipo = '" + equipo + "' and idpartido = " + idPartido +" and cambios.idjugador = jugador.pasaporte ";
+                  cmd.CommandType = CommandType.Text;
+                  MessageBox.Show(instruccion);
+                  OracleDataReader dr = cmd.ExecuteReader();
+
+                  string datos = "";
+                  //  dr.Read();
+                  while (dr.Read())
+                  {
+                      //datos += dr.GetString(0);
+                      aux += 0;
+                  }
+                  conn.Dispose();
+                  MessageBox.Show(aux.ToString());
+              }
+              catch (Exception m)
+              {
+                  MessageBox.Show("Error " + m);
+              }
+              return aux;*/
+
+            List<string> lista = new List<string>();
+            //string[] lista;
+            //lista.Add("");
+            //lista[0] = "d";
+            //MessageBox.Show(lista[0]);
             string oradb = "DATA SOURCE=localhost:1521/XE;USER ID=SYSTEM; Password = toma;";
-            int aux=0;
+            string valor = "";
             OracleConnection conn = new OracleConnection(oradb); // C#
 
             conn.Open();
 
-            try
-            {
-                OracleCommand cmd = new OracleCommand();
+            OracleCommand cmd = new OracleCommand();
 
-                cmd.Connection = conn;
-                string instruccion = " select cambios.idequipo from cambios,jugador where cambios.idequipo = '" + equipo + "' and idpartido = " + idPartido +" and cambios.idjugador = jugador.pasaporte ";
-                cmd.CommandText = instruccion; cmd.CommandType = CommandType.Text;
-                MessageBox.Show(instruccion);
-                OracleDataReader dr = cmd.ExecuteReader();
+            cmd.Connection = conn;
 
-                string datos = "";
-                //  dr.Read();
-                while (dr.Read())
-                {
-                    //datos += dr.GetString(0);
-                    aux += 0;
-                }
-                conn.Dispose();
-                MessageBox.Show(aux.ToString());
-            }
-            catch (Exception m)
+            cmd.CommandText = "select cambios.idequipo from cambios,jugador where cambios.idequipo = '" + equipo + "' and idpartido = " + idPartido +" and cambios.idjugador = jugador.pasaporte ";
+
+            cmd.CommandType = CommandType.Text;
+
+            OracleDataReader dr = cmd.ExecuteReader();
+
+            string valores = "";
+            int cont = 0;
+            while (dr.Read())
             {
-                MessageBox.Show("Error " + m);
+                valor += dr.GetString(0);
+                Console.WriteLine(dr.GetString(0));
+                MessageBox.Show(valor);
+                cont +=1;
+                MessageBox.Show(cont.ToString());
             }
-            return aux;
+            conn.Dispose();
+            return cont;
 
 
 
@@ -145,12 +181,13 @@ namespace proyecto2
             ExJ1 = existenciaJugador(equipo,Jsaliente);
             ExJ2 = existenciaJugador(equipo, Jentrante);
             int auxC = cantidadCambio(equipo);
-            if (ExJ1 && ExJ2 && (auxC<= 3 ))
+            MessageBox.Show("cantidad         "+auxC.ToString());
+            if (ExJ1 && ExJ2 && (auxC< 3 ))
             {
                 cont++;
-                string instruccion = "insert into cambios values( "+cont+", '"+ Jsaliente+"',"+idPartido+",'"+minutoA+"', '"+segundoA+"','"+Jentrante+"','"+equipo+"' )";
+                string instruccion = "insert into cambios values( "+auxC+", '"+ Jsaliente+"',"+idPartido+",'"+minutoA+"', '"+segundoA+"','"+Jentrante+"','"+equipo+"' )";
                 registrarCambio(instruccion);
-                MessageBox.Show("Exito!!");
+               // MessageBox.Show("Exito!!");
 
             }else
             {
@@ -165,7 +202,10 @@ namespace proyecto2
                     MessageBox.Show("Error el dato ingresado del jugador entrante es incorrecto");
                 }
 
-
+                if(auxC >=  3)
+                {
+                    MessageBox.Show("Ya se realizaron todos los cambios disponibles para el partido "+ equipo);
+                }
 
             }
 
